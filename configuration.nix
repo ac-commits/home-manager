e655b566c60e148e64816dbe752343bc1c3fcb1d
Itrekr <itrekr@oscar.cc>
2024-11-09 15:42:53 +0100

    Added laptop powersaving settings.

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,30 @@
   networking.hostName = "sleipnir";
   networking.networkmanager.enable = true; # Enable NetworkManager for easy network management
 
+  # Laptop battery setup
+  services.system76-scheduler.settings.cfsProfiles.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    };
+  };
+  services.power-profiles-daemon.enable = false;
+  powerManagement.powertop.enable = true;
+  services.thermald.enable = true;
+
+  # Laptop lid settings
+  services.logind = {
+    lidSwitch = "suspend";
+    lidSwitchDocked = "ignore";
+    powerKey = "poweroff";
+    suspendKey = "suspend";
+    hibernateKey = "hibernate";
+  };
+
   # Bluetooth setup
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
