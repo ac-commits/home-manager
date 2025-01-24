f3992a71f43bc3e658a32e099494931cc4090655
ac-commits <cromalex@proton.me>
2025-01-24 12:12:02 +0100

    add NVIDIA configuration

--- configuration.nix
+++ configuration.nix
@@ -98,7 +98,18 @@
     LC_TIME = "nl_NL.UTF-8";
   };
 
-  # Enable the X server and set up i3
+
+  # NVIDIA
+  hardware.graphics.enable = true;
+  hardware.nvidia.open = false;
+  hardware.nvidia.prime = {
+      offload.enable = true;
+      offload.enableOffloadCmd = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+  };
+
+# Enable the X server and set up i3
   services.xserver = {
     enable = true;
     xkb.layout = "us";
@@ -107,6 +118,7 @@
     autoRepeatInterval = 30;
     displayManager.lightdm.enable = true;
     windowManager.i3.enable = true;
+    videoDrivers = [ "nvidia" ];
   };
 
   # Define users
